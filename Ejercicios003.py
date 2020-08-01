c3e5b1208779c5222ef33f5342a64520ae60a7b7
Andrea Mena <65270657+MenMor@users.noreply.github.com>
2020-07-31 23:07:51 -0500

    Initial commit

---
+++ Ejercicios003.py
@@ -0,0 +1,132 @@
+"""Dada una medida de tiempo expresada en horas, minutos y segundos con valores aleatorios.
+    Elabore un programa que transforme dicha medida en una expresión correcta. """
+
+horas = int(input("Ingrese horas: "))
+minutos = int(input("Ingrese minutos: "))
+segundos = int(input("Ingrese segundos: "))
+dias = 0
+if segundos > 60:
+    minutos = minutos + (segundos // 60)
+    segundos = segundos % 60
+    print(segundos, " seg")
+else:
+    print(segundos, " seg")
+
+if minutos > 60:
+    horas = horas + (minutos // 60)
+    minutos = minutos % 60
+    print(minutos, " min")
+else:
+    print(minutos, " min")
+
+print(horas, " h")
+
+if horas > 24:
+    dias = horas // 24
+    print(dias, " dias")
+
+print(f"{dias} dias {horas} horas {minutos} min {segundos} segundos")
+
+
+# Con while
+
+horas = int(input("Introduce horas:"))
+min = int(input(" Introduce minutos"))
+segundos = int(input("Introduce segundos"))
+
+cont = 0
+cont2 = 0
+while segundos > 60:
+     segundos -= 60
+     cont += 1
+min += cont
+while min > 60:
+     min -= 60
+     cont2 += 1
+horas += cont2
+dias = horas//24
+while horas > 24:
+     horas -= 24
+
+print(f"{dias} dias {horas} horas {min} min {segundos} segundos ")
+
+
+# Promedio de 3 notas
+n = int(input("Ingrese nota 1: "))
+n2 = int(input("Ingrese nota 2: "))
+n3 = int(input("Ingrese nota 3: "))
+promedio = (n+n2+n3) / 3
+print(promedio)
+
+# Area de un triangulo
+altura = int(input("Ingrese altura: "))
+base = int(input("Ingrese base: "))
+area = (base * altura) /2
+print("El area es: ", area)
+
+"""cedula = [1,7,1,4,1,3,1,3,4,9]
+suma_impares = cedula[0]+cedula[2]+cedula[4]+cedula[6]+ cedula[8]
+suma_pares = cedula[1]+cedula[3]+cedula[5]+cedula[7]
+# Acceder elementos de lista
+digitos_pares = cedula[1:8:2]
+digitos_impares = cedula[0:9:2]"""
+
+# Factorial
+numero = int(input("Ingrese valor para calcular su factorial: "))
+total = 1
+for i in range(1, numero+1):
+     total *= i
+print(total)
+
+
+# Realizar un programa que calculé el dígito verificador de la cédula ecuatoriana.
+
+print("Introduce 8 digitos de cédula")
+sumaPar = 0
+sumaImpar = 0
+i = 1
+while i < 10:
+     digitos = int(input("Introduce dígito: "))
+     if i % 2 == 0:
+          sumaPar += digitos
+     else:
+          prod = digitos * 2
+     if prod > 9:
+          prod -= 9
+     sumaImpar += prod
+     i += 1
+
+sumaT = sumaPar + sumaImpar
+mod = sumaT % 10
+if mod != 0:
+     mod = 10 - mod
+     print(f"Dígito verificador es : {mod}")
+else:
+     print("Dígito verificador es: 0 ")
+
+
+
+# Factura con descuento del 10% si su monto es mayor a $100
+subtotal = 0
+i = 1
+while i != 0:
+     total_productos = int(input("Ingrese total de productos: "))
+     j = 1
+     while j <= total_productos:
+         producto = int(input("Valor producto : "))
+         subtotal += producto
+         j += 1
+     fin_while = int(input(" digite 0 para terminar proceso:"))
+     if fin_while == 0:
+          i = 0
+
+if subtotal > 100:
+    descuento = subtotal * 0.10
+    subtotal -= descuento
+
+iva = subtotal * 0.12
+iva = round(iva, 2)
+total_pago = subtotal + iva
+total_pago = round(total_pago, 2)
+
+print(f"IVA: {iva} Total a pagar: {total_pago}")
---
+++ Tarea01.py
@@ -0,0 +1,214 @@
+import math
+
+# Convertir de grados a radianes.
+print("Convertir de grados a radianes.")
+
+grados = int(input("Ingrese valor de grados a convertir: "))
+rad = grados * 3.1416 / 180
+print("Grados", grados, " a ", rad, " rad")
+
+
+""" Realizar un algoritmo que pida al usuario la velocidad en m/s y el radio de la
+    circunferencia de la pista, el resultado el programa devuelve el tiempo que tarda el
+    atleta en dar dos vueltas a la pista, sabiendo que el atleta descansa 1min cada 1000m. """
+
+print("\n Calculo de tiempo que tarda un atlta en dar dos vueltas a la pista.")
+
+velocidad = float(input("Introduce velocidad en m/s: "))
+radio = float(input("Introduce radio de pista: "))
+
+velocidad_angular = velocidad/radio  # velocidad angular para fórmula periodo de mcu.
+tiempo = (2*3.1416/velocidad_angular)*2
+longitud = 2*3.1416*radio
+
+if longitud > 2000:
+    tiempo += 120
+
+print(f"Tiempo : {tiempo}")
+
+
+# Realizar un programa que calculé el dígito verificador de la cédula ecuatoriana.
+
+""" Elaborar un algoritmo que calculé el impuesto a la renta con relación de dependencia de un empleado,
+    conociendo el sueldo mensual del funcionario, sus gastos deducibles del año (Salud, vivienda, alimentación, 
+    educación y vestimenta) y el porcentaje de aportación al IESS. Debe basarse en la tabla de Impuesto a la Renta 
+    del 2020 para si cálculo."""
+
+print("\n Calculo impuesto a la renta con relación de dependencia de un empleado.")
+
+sueldo = float(input("Introduce sueldo mensual de funcionario  "))
+gastos = float(input("Introduce total de gastos de Salud, vivienda, alimentación, educación y vestimenta  "))
+
+if gastos <= sueldo*0.4:
+
+    sueldo *= 12
+    aporte_iess = sueldo * 9.45
+    aporte_iess = (round(aporte_iess, 2))  # limito a dos decimales
+    sueldo = aporte_iess - sueldo - gastos
+    sueldo = (round(sueldo, 2))
+
+    if sueldo <= 11315.00:
+        impuesto = 0
+
+    elif (sueldo >= 11315.01) and (sueldo <= 14416.00):
+        impuesto = sueldo * 0.05
+
+    elif (sueldo >= 14416.01) and (sueldo <= 18018.00):
+        impuesto = sueldo * 0.10 + 155
+
+    elif (sueldo >= 18018.01) and (sueldo <= 21639.00):
+        impuesto = sueldo * 0.12 + 515
+
+    elif (sueldo >= 21639.01) and (sueldo <= 43268.00):
+        impuesto = sueldo * 0.15 + 950
+
+    elif (sueldo >= 43268.01) and (sueldo <= 64887.00):
+        impuesto = sueldo * 0.20 + 4194
+
+    elif (sueldo >= 64887.01) and (sueldo <= 86516.00):
+        impuesto = sueldo * 0.25 + 8518
+
+    elif (sueldo >= 86516.01) and (sueldo <= 115338.00):
+        impuesto = sueldo * 0.30 + 13925
+
+    else:
+        impuesto = sueldo * 0.35 + 22572
+
+    impuesto = round(impuesto, 2)  # Limito a 2 decimales
+    print(f"Impuesto a la renta: {impuesto}")
+
+else:
+    print("Los gastos sobrepasan el porcentaje de sueldo")
+
+
+
+# Resolucion ecuacion cuadratica ax^2 + bx + c = 0 por formula general
+
+print("\nResolucion ecuacion cuadratica ax^2 + bx + c = 0 por formula general.")
+a = int(input("Valor de a: "))
+b = int(input("Valor de b: "))
+c = int(input("Valor de c: "))
+
+discriminante = b ** 2 - 4 * a * c
+
+if discriminante < 0:
+    print("La ecuacion no tiene soluciones reales")
+elif discriminante == 0:
+    x = -b / 2*a
+    print(f"Solucion unica: {x}")
+else:
+    x1 = (-b - math.sqrt(discriminante)) / (2*a)
+    x2 = (-b + math.sqrt(discriminante)) / (2*a)
+    print(f"Dos soluciones reales: x1= {x1} x2= {x2}")
+
+
+
+"""Dada una medida de tiempo expresada en horas, minutos y segundos con valores aleatorios.
+   Elabore un programa que transforme dicha medida en una expresión correcta."""
+
+print("\nMedida de tiempo.")
+
+horas = int(input("Ingrese horas: "))
+minutos = int(input("Ingrese minutos: "))
+segundos = int(input("Ingrese segundos: "))
+dias = 0
+if segundos > 60:
+    minutos = minutos + (segundos // 60)
+    segundos = segundos % 60
+    print(segundos, " seg")
+else:
+    print(segundos, " seg")
+
+if minutos > 60:
+    horas = horas + (minutos // 60)
+    minutos = minutos % 60
+    print(minutos, " min")
+else:
+    print(minutos, " min")
+
+print(horas, " h")
+
+if horas > 24:
+    dias = horas // 24
+    print(dias, " dias")
+
+print(f"{dias} dias {horas} horas {minutos} min {segundos} segundos")
+
+
+# Factura con descuento del 10% si su monto es mayor a $100
+
+print("\nFactura con descuento del 10% si su monto es mayor a $100.")
+
+producto1 = int(input("Valor producto 1: "))
+producto2 = int(input("Valor producto 2: "))
+producto3 = int(input("Valor producto 3: "))
+
+subtotal = producto1 + producto2 + producto3
+if subtotal > 100:
+    descuento = subtotal * 0.10
+    subtotal -= descuento
+
+iva = subtotal * 0.12
+total_pago = subtotal + iva
+
+print(f"IVA: {iva} Total a pagar: {total_pago}")
+
+
+
+# Si el promedio es mayor a 7 el alumno esta aprobaado caso contario estara reprobado
+
+print("\nSi el promedio es mayor a 7 el alumno esta aprobaado caso contario estara reprobado.")
+
+nota1 = float(input("Ingrese primera nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+
+suma_notas = (nota1 + nota2) / 2
+
+if suma_notas >= 7:
+     print("Materia aprobada")
+else:
+     print("Materia Reprobada")
+
+# algoritmo que simula una calculadora basica (menu para que usuario indique que operacion desea)
+print("\nCalculadora basica, elija su opcion.")
+
+opcion = int(input("1. Suma" "\n2. Resta" "\n3. Multiplicacion" "\n4. Division" "\n:  "))
+
+if (opcion == 1) or (opcion == 2) or (opcion == 3) or (opcion == 4):
+
+    numero1 = float(input("Ingrese primer valor: "))
+    numero2 = float(input("Ingrese segundo valor: "))
+    resultado = 0
+
+    if opcion == 1:
+        resultado = numero1 + numero2
+
+    elif opcion == 2:
+        resultado = numero1 - numero2
+
+    elif opcion == 3:
+        resultado = numero1 * numero2
+
+    else:
+        if numero2 == 0:
+            print("Error division sobre 0")
+        else:
+            resultado = numero1 / numero2
+
+    print("Respuesta: ", resultado)
+
+else:
+    print("No existe esa opcion")
+
+
+# Algoritmo que determina si un numero entero es divisible para otro
+
+numero_uno = int(input("Ingrese primer numero: "))
+numero_dos = int(input("Ingrese segundo numero: "))
+
+divisible = numero_uno % numero_dos
+
+if divisible == 0:
+    print("Es divisible: ", numero_uno / numero_dos)
+else:
+    print("No es divisible")
