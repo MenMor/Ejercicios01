0b74c2b8e9530f46abb1b2475622f1fadeb45c4c
Andrea Mena <65270657+MenMor@users.noreply.github.com>
2020-08-19 13:21:10 -0500

    01

---
+++ vector.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+lim = int(input("Ingrese limite: "))
+suma = 0
+leibniz = np.arange(0, lim)
+suma += np.sum(((-1)**leibniz) / (2*leibniz+1))
+print(f"pi = {suma*4}")
+
+
+suma_pi = 0
+pi = np.arange(0, 1000)
+suma_pi += np.sum(((-1)**pi) / (2*pi+1))
+print(f"pi = {suma_pi*4}")
